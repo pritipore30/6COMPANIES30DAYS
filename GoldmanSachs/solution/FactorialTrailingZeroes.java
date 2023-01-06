a8561ee68e6b840bdce0857369f5a689856047a0
pritipore30 <pritipore2001@gmail.com>
2023-01-06 20:28:44 +0530

    Day6 GoldmanSachs

---
+++ GoldmanSachs/solution/FactorialTrailingZeroes.java
@@ -0,0 +1,16 @@
+package solution;
+
+public class FactorialTrailingZeroes {
+	public static int trailingZeroes(int n) {
+        int count = 0;
+        for(int i = 5; n / i >= 1; i*=5){
+            int temp = n /i;
+            count += temp;
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println(trailingZeroes(n));
+	}
+}
--- GoldmanSachs/solution/ValidSquare.java
+++ GoldmanSachs/solution/ValidSquare.java
@@ -1,5 +1,32 @@
 package solution;
 
-public class ValidSquare {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Validsquare {
+	public static boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+		Set<Integer> dist = new HashSet<>();
+		dist.add(distance(p1, p2));
+		dist.add(distance(p1, p3));
+		dist.add(distance(p1, p4));
+		dist.add(distance(p2, p3));
+		dist.add(distance(p2, p4));
+		dist.add(distance(p3, p4));
+		return !dist.contains(0) && dist.size() == 2;
+
+	}
+
+	public static int distance(int[] a, int[] b) {
+		return (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
+	}
+
+	public static void main(String[] args) {
+		int[] p1 = { 0, 0 };
+		int[] p2 = { 1, 1 };
+		int[] p3 = { 1, 0 };
+		int[] p4 = { 0, 1 };
+		boolean result = validSquare(p1, p2, p3, p4);
+		System.out.println(result);
+	}
 
 }
