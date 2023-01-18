765c887b90e6685508657f4ce30f3561ebe7bf65
pritipore30 <pritipore2001@gmail.com>
2023-01-18 20:53:14 +0530

    Day18 Flipkart

---
+++ Flipkart/solution/ClosestPrimeNumbersinRange.java
@@ -0,0 +1,53 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClosestPrimeNumbersinRange {
+	public static int[] closestPrimes(int left, int right) {
+	    final boolean[] isPrime = findprime(right + 1);
+	    List<Integer> primes = new ArrayList<>();
+
+	    for (int i = left; i <= right; ++i)
+	      if (isPrime[i])
+	        primes.add(i);
+
+	    if (primes.size() < 2)
+	      return new int[] {-1, -1};
+
+	    int minDiff = Integer.MAX_VALUE;
+	    int num1 = -1;
+	    int num2 = -1;
+
+	    for (int i = 1; i < primes.size(); ++i) {
+	      final int diff = primes.get(i) - primes.get(i - 1);
+	      if (diff < minDiff) {
+	        minDiff = diff;
+	        num1 = primes.get(i - 1);
+	        num2 = primes.get(i);
+	      }
+	    }
+
+	    return new int[] {num1, num2};
+	  }
+
+	  public static boolean[] findprime(int n) {
+	    boolean[] isPrime = new boolean[n];
+	    Arrays.fill(isPrime, true);
+	    isPrime[0] = false;
+	    isPrime[1] = false;
+	    for (int i = 2; i * i < n; ++i)
+	      if (isPrime[i])
+	        for (int j = i * i; j < n; j += i)
+	          isPrime[j] = false;
+	    return isPrime;
+	  }
+	public static void main(String[] args) {
+		int left = 10; 
+		int right = 19;
+		int[] result = closestPrimes(left,right);
+		System.out.println(Arrays.toString(result));
+	}
+
+}
