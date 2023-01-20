e1cda728142f6c3df6ced2ca89fcd103202e8ef1
pritipore30 <pritipore2001@gmail.com>
2023-01-20 20:58:42 +0530

    Day 20 Flipkart

---
+++ Flipkart/solution/CustomSortString.java
@@ -0,0 +1,29 @@
+package solution;
+
+public class CustomSortString {
+	public static String customSortString(String order, String s) {
+		StringBuilder sb = new StringBuilder();
+		int[] count = new int[128];
+
+		for (final char c : s.toCharArray())
+			++count[c];
+
+		for (final char c : order.toCharArray())
+			while (count[c]-- > 0)
+				sb.append(c);
+
+		for (char c = 'a'; c <= 'z'; ++c)
+			while (count[c]-- > 0)
+				sb.append(c);
+
+		return sb.toString();
+
+	}
+
+	public static void main(String[] args) {
+		String order = "cba";
+		String s = "abcd";
+		System.out.println(customSortString(order, s));
+	}
+
+}
